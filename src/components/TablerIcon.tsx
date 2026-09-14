f3e2904c80a1cf5c4561c40bba7f424d82607fac
Aser Nasr <asernasr@figranium.dev>
2026-09-14 14:52:24 +0300

    Expand Tabler icon coverage

--- src/components/TablerIcon.tsx
+++ src/components/TablerIcon.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import type { SVGProps } from "react";
+import type { ReactNode, SVGProps } from "react";
 import clsx from "clsx";
 
-type IconDefinition = React.ReactNode;
+type IconDefinition = ReactNode;
 
 const icons: Record<string, IconDefinition> = {
   login: <><path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" /><path d="M20 12h-13l3 -3m0 6l-3 -3" /></>,
@@ -31,6 +31,27 @@ const icons: Record<string, IconDefinition> = {
   expand_more: <path d="M6 9l6 6l6 -6" />,
   chevron_left: <path d="M15 6l-6 6l6 6" />,
   badge_check: <><path d="M12 3l2 2l3 -.5l.5 3l2 2l-2 2l.5 3l-3 .5l-2 2l-2 -2l-3 .5l-.5 -3l-2 -2l2 -2l-.5 -3l3 -.5z" /><path d="M9 12l2 2l4 -4" /></>,
+  alert_circle: <><circle cx="12" cy="12" r="9" /><path d="M12 8v4m0 4h.01" /></>,
+  file_upload: <><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M5 3h9l5 5v13h-14z" /><path d="M12 17v-6m-3 3l3 -3l3 3" /></>,
+  send: <><path d="M10 14l11 -11" /><path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1z" /></>,
+  photo: <><rect x="3" y="3" width="18" height="18" rx="2" /><circle cx="8.5" cy="8.5" r="1.5" /><path d="M21 15l-5 -5l-11 11" /></>,
+  upload: <><path d="M12 16v-12m-4 4l4 -4l4 4" /><path d="M4 16v4h16v-4" /></>,
+  terminal_2: <><path d="M4 17l6 -6l-6 -6" /><path d="M12 19h8" /></>,
+  arrow_back: <><path d="M5 12h14" /><path d="M9 16l-4 -4l4 -4" /></>,
+  book: <><path d="M4 5a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v14a1 1 0 0 1 -1.5 .86l-6.5 -3.86l-6.5 3.86a1 1 0 0 1 -1.5 -.86z" /><path d="M12 6v10" /></>,
+  hierarchy: <><circle cx="12" cy="5" r="2" /><circle cx="6" cy="19" r="2" /><circle cx="18" cy="19" r="2" /><path d="M12 7v5m0 0h-6v5m6 -5h6v5" /></>,
+  braces: <><path d="M7 3h-1a2 2 0 0 0 -2 2v5a2 2 0 0 1 -2 2a2 2 0 0 1 2 2v5a2 2 0 0 0 2 2h1" /><path d="M17 3h1a2 2 0 0 1 2 2v5a2 2 0 0 0 2 2a2 2 0 0 0 -2 2v5a2 2 0 0 1 -2 2h-1" /></>,
+  target: <><circle cx="12" cy="12" r="9" /><circle cx="12" cy="12" r="5" /><circle cx="12" cy="12" r="1" /></>,
+  device_desktop: <><rect x="3" y="4" width="18" height="13" rx="1" /><path d="M8 21h8m-4 -4v4" /></>,
+  webhook: <><path d="M6 12a3 3 0 1 1 3 -3" /><path d="M15 9a3 3 0 1 1 3 3" /><path d="M12 15a3 3 0 1 1 -3 3" /><path d="M9 9l6 0m3 3l-3 6m-6 0l-3 -6" /></>,
+  robot: <><rect x="4" y="7" width="16" height="13" rx="2" /><path d="M9 12h.01m6 0h.01m-6 4h6" /><path d="M12 7v-3m-2 0h4" /></>,
+  bug: <><path d="M9 9h6v8a3 3 0 0 1 -6 0z" /><path d="M12 4v5m-4 -3l2 2m6 -2l-2 2m-8 4h3m6 0h3m-12 4h3m6 0h3" /></>,
+  save: <><path d="M5 4h12l2 2v14h-14z" /><path d="M8 4v6h8v-6m-8 16v-6h8v6" /></>,
+  copy: <><rect x="8" y="8" width="12" height="12" rx="2" /><path d="M16 8v-4h-12v12h4" /></>,
+  check: <path d="M5 12l5 5l9 -9" />,
+  external_link: <><path d="M10 6h-5v13h13v-5" /><path d="M13 5h6v6m0 -6l-9 9" /></>,
+  lock: <><rect x="5" y="10" width="14" height="11" rx="2" /><path d="M8 10v-3a4 4 0 0 1 8 0v3" /></>,
+  user: <><circle cx="12" cy="8" r="4" /><path d="M6 21v-2a6 6 0 0 1 12 0v2" /></>,
 };
 
 const aliases: Record<string, string> = {
@@ -49,7 +70,23 @@ const aliases: Record<string, string> = {
   pencil: "edit",
   trash: "delete",
   world: "public",
+  language: "public",
   chevron_down: "expand_more",
+  error: "alert_circle",
+  upload_file: "file_upload",
+  publish: "send",
+  image: "photo",
+  output: "terminal_2",
+  menu_book: "book",
+  account_tree: "hierarchy",
+  data_object: "braces",
+  my_location: "target",
+  table_chart: "braces",
+  monitor: "device_desktop",
+  smart_toy: "robot",
+  bug_report: "bug",
+  content_copy: "copy",
+  open_in_new: "external_link",
 };
 
 interface TablerIconProps extends SVGProps<SVGSVGElement> {
